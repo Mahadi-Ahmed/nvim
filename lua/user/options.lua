b2067856b693c9a6e10fede8b9a58e3ddb717426
Mahadi Ahmed <mahadi4@gmail.com>
2022-10-25 00:50:58 +0200

    add options lua & some basic config options

---
+++ lua/user/options.lua
@@ -0,0 +1,4 @@
+vim.opt.clipboard = "unnamedplus" -- allows neovim to access the system clipboard
+vim.opt.fileencoding = "utf-8" -- the encoding written to a file
+vim.opt.relativenumber = true
+vim.opt.hlsearch = true -- highlight all matches on previous search pattern
