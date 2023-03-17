eb229b059e25bb6ec4aeb1a3edae2f712fdf8cb1
Mahadi-Ahmed <mahadi4@gmail.com>
2023-03-18 00:26:44 +0100

    rename lsp to lspZero

--- init.lua
+++ init.lua
@@ -9,10 +9,11 @@ require 'mahadia.plugins.nvim-tree'
 require 'mahadia.plugins.lualine'
 require 'mahadia.plugins.telescope'
 -- require 'mahadia.plugins.cmp'
-require 'mahadia.plugins.lsp'
+require 'mahadia.plugins.lspZero'
 require 'mahadia.plugins.harpoon'
 require 'mahadia.plugins.autopairs'
 require 'mahadia.plugins.toggleterm'
+-- require 'mahadia.plugins.undotree'
 
 local function open_nvim_tree()
 
