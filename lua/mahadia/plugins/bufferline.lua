b86751c4c879c5584a9cde7aab793006932f4a34
Mahadi-Ahmed <mahadi4@gmail.com>
2023-03-20 16:16:30 +0100

    add basic config for bufferline

--- lua/mahadia/plugins/bufferline.lua
+++ lua/mahadia/plugins/bufferline.lua
@@ -1,6 +1,63 @@
-local status_ok, bufferline = pcall(require, "bufferline")
+local status_ok, bufferline = pcall(require, 'bufferline')
 if not status_ok then
   return
 end
 
-bufferline.setup{}
+bufferline.setup {
+  options = {
+    mode = 'buffers',
+    close_command = "bdelete! %d",       -- can be a string | function, see "Mouse actions"
+    right_mouse_command = "bdelete! %d", -- can be a string | function, see "Mouse actions"
+    left_mouse_command = "buffer %d",    -- can be a string | function, see "Mouse actions"
+    middle_mouse_command = nil,          -- can be a string | function, see "Mouse actions"
+    indicator = {
+      icon = '▎',                      -- this should be omitted if indicator style is not 'icon'
+      style = 'icon',                    -- style = 'icon' | 'underline' | 'none',
+    },
+    buffer_close_icon = '',
+    modified_icon = '●',
+    close_icon = '',
+    left_trunc_marker = '',
+    right_trunc_marker = '',
+    max_name_length = 18,
+    max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
+    truncate_names = true,  -- whether or not tab names should be truncated
+    tab_size = 18,
+    diagnostics = "nvim_lsp",
+    diagnostics_update_in_insert = false,
+    offsets = {
+      {
+        filetype = "undotree",
+        text = "Undotree",
+        highlight = "PanelHeading",
+        padding = 1,
+      },
+      {
+        filetype = "NvimTree",
+        text = "Explorer",
+        highlight = "PanelHeading",
+        padding = 1,
+      },
+      {
+        filetype = "DiffviewFiles",
+        text = "Diff View",
+        highlight = "PanelHeading",
+        padding = 1,
+      },
+      {
+        filetype = "packer",
+        text = "Packer",
+        highlight = "PanelHeading",
+        padding = 1,
+      },
+    },
+    color_icons = true,
+    show_buffer_icons = true,        -- disable filetype icons for buffers
+    show_buffer_close_icons = false,
+    show_buffer_default_icon = true, -- whether or not an unrecognised filetype should show a default icon
+    show_close_icon = false,
+    show_tab_indicators = true,
+    show_duplicate_prefix = true, -- whether to show duplicate buffer prefix
+    separator_style = "thin",
+  }
+}
