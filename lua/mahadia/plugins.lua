f741f60e14dadde43329009e3c22ea94ce4afe2a
mahadia <mahadi.ahmed@postnord.com>
2024-07-04 15:59:51 +0200

    upgrade telescope & lazy lock

--- lua/mahadia/plugins.lua
+++ lua/mahadia/plugins.lua
@@ -71,7 +71,7 @@ local plugins = {
   -- fuzzy finding w/ telescope
   {
     'nvim-telescope/telescope.nvim',
-    tag = '0.1.4',
+    tag = '0.1.8',
     dependencies = { { 'nvim-lua/plenary.nvim' }, { 'nvim-telescope/telescope-fzf-native.nvim' } }
   },
   { "nvim-telescope/telescope-fzf-native.nvim",    build = "make",                                  lazy = true },
--- lua/mahadia/plugins/telescope.lua
+++ lua/mahadia/plugins/telescope.lua
@@ -11,101 +11,101 @@ if not actions_setup then
 end
 
 telescope.setup({
-    ---@usage disable telescope completely [not recommended]
-    active = true,
-    on_config_done = nil,
-    theme = "dropdown", ---@type telescope_themes
-    defaults = {
-      entry_prefix = "  ",
-      initial_mode = "insert",
-      selection_strategy = "reset",
-      sorting_strategy = nil,
-      layout_strategy = nil,
-      layout_config = {},
-      vimgrep_arguments = {
-        "rg",
-        "--color=never",
-        "--no-heading",
-        "--with-filename",
-        "--line-number",
-        "--column",
-        "--smart-case",
-        "--hidden",
-        "--glob=!.git/",
+  ---@usage disable telescope completely [not recommended]
+  active = true,
+  on_config_done = nil,
+  -- theme = "dropdown", ---@type telescope_themes
+  defaults = {
+    entry_prefix = "  ",
+    initial_mode = "insert",
+    selection_strategy = "reset",
+    sorting_strategy = nil,
+    layout_strategy = nil,
+    layout_config = {},
+    vimgrep_arguments = {
+      "rg",
+      "--color=never",
+      "--no-heading",
+      "--with-filename",
+      "--line-number",
+      "--column",
+      "--smart-case",
+      "--hidden",
+      "--glob=!.git/",
+    },
+    ---@usage Mappings are fully customizable. Many familiar mapping patterns are setup as defaults.
+    mappings = {
+      i = {
+        ["<C-n>"] = actions.move_selection_next,
+        ["<C-p>"] = actions.move_selection_previous,
+        ["<C-c>"] = actions.close,
+        ["<C-j>"] = actions.cycle_history_next,
+        ["<C-k>"] = actions.cycle_history_prev,
+        ["<C-q>"] = function(...)
+          actions.smart_send_to_qflist(...)
+          actions.open_qflist(...)
+        end,
+        ["<CR>"] = actions.select_default,
       },
-      ---@usage Mappings are fully customizable. Many familiar mapping patterns are setup as defaults.
+      n = {
+        ["<C-n>"] = actions.move_selection_next,
+        ["<C-p>"] = actions.move_selection_previous,
+        ["<C-q>"] = function(...)
+          actions.smart_send_to_qflist(...)
+          actions.open_qflist(...)
+        end,
+      },
+    },
+    file_ignore_patterns = { "lock" },
+    path_display = { "smart" },
+    winblend = 0,
+    border = {},
+    borderchars = nil,
+    color_devicons = true,
+    set_env = { ["COLORTERM"] = "truecolor" },   -- default = nil,
+  },
+  pickers = {
+    find_files = {
+      hidden = true,
+    },
+    live_grep = {
+      --@usage don't include the filename in the search results
+      only_sort_text = true,
+    },
+    grep_string = {
+      only_sort_text = true,
+    },
+    buffers = {
+      initial_mode = "normal",
       mappings = {
         i = {
-          ["<C-n>"] = actions.move_selection_next,
-          ["<C-p>"] = actions.move_selection_previous,
-          ["<C-c>"] = actions.close,
-          ["<C-j>"] = actions.cycle_history_next,
-          ["<C-k>"] = actions.cycle_history_prev,
-          ["<C-q>"] = function(...)
-            actions.smart_send_to_qflist(...)
-            actions.open_qflist(...)
-          end,
-          ["<CR>"] = actions.select_default,
+          ["<C-d>"] = actions.delete_buffer,
         },
         n = {
-          ["<C-n>"] = actions.move_selection_next,
-          ["<C-p>"] = actions.move_selection_previous,
-          ["<C-q>"] = function(...)
-            actions.smart_send_to_qflist(...)
-            actions.open_qflist(...)
-          end,
+          ["dd"] = actions.delete_buffer,
         },
       },
-      file_ignore_patterns = { "lock" },
-      path_display = { "smart" },
-      winblend = 0,
-      border = {},
-      borderchars = nil,
-      color_devicons = true,
-      set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
     },
-    pickers = {
-      find_files = {
-        hidden = true,
-      },
-      live_grep = {
-        --@usage don't include the filename in the search results
-        only_sort_text = true,
-      },
-      grep_string = {
-        only_sort_text = true,
-      },
-      buffers = {
-        initial_mode = "normal",
-        mappings = {
-          i = {
-            ["<C-d>"] = actions.delete_buffer,
-          },
-          n = {
-            ["dd"] = actions.delete_buffer,
-          },
-        },
-      },
-      planets = {
-        show_pluto = true,
-        show_moon = true,
-      },
-      git_files = {
-        hidden = false,
-        show_untracked = true,
-      },
-      colorscheme = {
-        enable_preview = true,
-      },
+    planets = {
+      show_pluto = true,
+      show_moon = true,
     },
-    extensions = {
-      fzf = {
-        fuzzy = true, -- false will only do exact matching
-        override_generic_sorter = true, -- override the generic sorter
-        override_file_sorter = true, -- override the file sorter
-        case_mode = "smart_case", -- or "ignore_case" or "respect_case"
-      },
+    git_files = {
+      hidden = false,
+      show_untracked = true,
     },
+    colorscheme = {
+      enable_preview = true,
+    },
+  },
+  extensions = {
+    fzf = {
+      fuzzy = true,                     -- false will only do exact matching
+      override_generic_sorter = true,   -- override the generic sorter
+      override_file_sorter = true,      -- override the file sorter
+      case_mode = "smart_case",         -- or "ignore_case" or "respect_case"
+    },
+  },
 })
 
 require("telescope").load_extension "fzf"
