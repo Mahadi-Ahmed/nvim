99a7e9a6d18bfc419c7be5994a93baf37d9bc6b8
mahadia <mahadi.ahmed@postnord.com>
2025-04-02 16:12:42 +0200

    add oil nvim

--- lua/mahadia/plugins.lua
+++ lua/mahadia/plugins.lua
@@ -90,6 +90,15 @@ local plugins = {
       require('mahadia.plugins.nvim-tree')
     end
   },
+  {
+    'stevearc/oil.nvim',
+    opts = {},
+    -- Optional dependencies
+    dependencies = { { "echasnovski/mini.icons", opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    cmd = 'Oil'
+  },
   -- commenting with gc
   {
     "numToStr/Comment.nvim",
